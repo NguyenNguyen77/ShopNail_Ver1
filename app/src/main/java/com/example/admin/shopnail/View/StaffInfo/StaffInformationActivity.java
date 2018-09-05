05b91a012bf230ce5bae45966888361899feaa58
KhoaNguyen8076 <ndkhoa17@gmail.com>
2018-09-06 00:12:25 +0700

    [KhoaNguyen] Add menu. Not completed.

--- app/src/main/java/com/example/admin/shopnail/View/StaffInfo/StaffInformationActivity.java
+++ app/src/main/java/com/example/admin/shopnail/View/StaffInfo/StaffInformationActivity.java
@@ -3,12 +3,21 @@ package com.example.admin.shopnail.View.StaffInfo;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.DrawableUtils;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.admin.shopnail.R;
 
@@ -49,5 +58,23 @@ public class StaffInformationActivity extends Activity implements View.OnClickLi
     @Override
     public void onClick(View view) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.search:
+                Toast.makeText(this, R.string.login_sucessfull, Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
