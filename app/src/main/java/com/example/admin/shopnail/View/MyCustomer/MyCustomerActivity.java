9eee6aea84a35a101047381f9dd17854eebed9fe
KhoaNguyen8076 <ndkhoa17@gmail.com>
2018-09-29 00:50:15 +0700

    [KhoaNguyen] Update UI My Customer

--- app/src/main/java/com/example/admin/shopnail/View/MyCustomer/MyCustomerActivity.java
+++ app/src/main/java/com/example/admin/shopnail/View/MyCustomer/MyCustomerActivity.java
@@ -2,8 +2,11 @@ package com.example.admin.shopnail.View.MyCustomer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.admin.shopnail.R;
@@ -13,7 +16,10 @@ import com.example.admin.shopnail.View.ViewManager;
 public class MyCustomerActivity extends Activity implements View.OnClickListener {
 
     protected ViewManager mViewManager = ViewManager.getInstance();
-    Button btn_back;
+    private Button mBtnBack;
+    private Button mBtnUpdateService;
+    private Button mBtnCancelService;
+    private ListView mLvMyCustomerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +28,67 @@ public class MyCustomerActivity extends Activity implements View.OnClickListener
 
         new NailActionBarGenerator().generate(this,
                 NailActionBarGenerator.BarType.MY_CUSTOMER);
-        btn_back = findViewById(R.id.btn_go_back);
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewManager.handleBackScreen();
-            }
-        });
 
+        mBtnBack = (Button) findViewById(R.id.btn_go_back);
+        mBtnUpdateService = (Button) findViewById(R.id.btn_update_service);
+        mBtnCancelService = (Button) findViewById(R.id.btn_cancel_service);
+        mLvMyCustomerList = (ListView)findViewById(R.id.list_my_customer);
+
+        mBtnBack.setOnClickListener(this);
+        mBtnUpdateService.setOnClickListener(this);
+        mBtnCancelService.setOnClickListener(this);
         mViewManager.setActivity(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_go_back:
-                Toast.makeText(this, "click select service", Toast.LENGTH_SHORT).show();
+                mViewManager.handleBackScreen();
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getActionBar().setIcon(R.drawable.ic_menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.action_menu_for_staff).setVisible(true);
+        menu.findItem(R.id.action_select_service).setVisible(true);
+        menu.findItem(R.id.action_manage_staff).setVisible(true);
+        menu.findItem(R.id.action_customer_service_history).setVisible(true);
+        menu.findItem(R.id.action_staff_info).setVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_menu_for_staff:
+                mViewManager.setView(ViewManager.VIEW_KEY.MENU_FOR_STAFF);
+                return true;
+            case R.id.action_select_service:
+                mViewManager.setView(ViewManager.VIEW_KEY.SELECT_SERVICE);
+                return true;
+            case R.id.action_manage_staff:
+                mViewManager.setView(ViewManager.VIEW_KEY.MANAGE_STAFF);
+                return true;
+            case R.id.action_customer_service_history:
+                mViewManager.setView(ViewManager.VIEW_KEY.CUSTOMER_SERVICE_HISTORY);
+                return true;
+            case R.id.action_staff_info:
+                mViewManager.setView(ViewManager.VIEW_KEY.STAFF_INFO);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
