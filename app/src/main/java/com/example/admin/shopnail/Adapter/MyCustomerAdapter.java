802b15ef62b295baeb757d5b102a481f3e62f1cb
NguyenNguyen <kongnguyen77@gmail.com>
2018-10-16 00:57:33 +0700

    Update UI
    - My customer: chữ trắng, vị trí check box, đổi màu data load,
    - Manage Staff: 2 button to quá

--- app/src/main/java/com/example/admin/shopnail/Adapter/MyCustomerAdapter.java
+++ app/src/main/java/com/example/admin/shopnail/Adapter/MyCustomerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.admin.shopnail.Adapter;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.admin.shopnail.Model.ServicesOfShop;
@@ -53,19 +55,28 @@ public class MyCustomerAdapter extends BaseAdapter {
 
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.item_my_customer_detail_service, null);
-            holder = new MyCustomerAdapter.ViewHolder();
-            holder.nameService = convertView.findViewById(R.id.txt_name_service);
-            holder.priceService = convertView.findViewById(R.id.txt_price_service);
-            txt_date = convertView.findViewById(R.id.txt_time_service);
-
-            convertView.setTag(holder);
         } else {
             holder = (MyCustomerAdapter.ViewHolder) convertView.getTag();
         }
+            holder = new MyCustomerAdapter.ViewHolder();
+            holder.nameService = convertView.findViewById(R.id.txt_name_service);
+            holder.priceService = convertView.findViewById(R.id.txt_price_service);
+            LinearLayout lvItem = (LinearLayout) convertView.findViewById(R.id.item_list_my_customer);
+            txt_date = convertView.findViewById(R.id.txt_time_service);
+
+            convertView.setTag(holder);
+
 
         holder.nameService.setText(listService.get(i).nameService);
 //        holder.priceService.setText((int) listService.get(i).priceService);
 
+        if (i % 2 == 1) {
+            int backgroundColor = ContextCompat.getColor(convertView.getContext(), R.color.list_1);
+            lvItem.setBackgroundColor(backgroundColor);
+        } else {
+            int backgroundColor = ContextCompat.getColor(convertView.getContext(), R.color.list_2);
+            lvItem.setBackgroundColor(backgroundColor);
+        }
         return convertView;
     }
 
